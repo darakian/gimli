52a461d3daf001bc965c392260bf85f029337622
Jon Moroney <darakian@gmail.com>
2019-12-05 16:45:22 -0800

    Remove old comments

--- src/main.rs
+++ src/main.rs
@@ -243,48 +243,4 @@ fn main() {
         file.write_all(nonce).expect("Error writing to output file");
         file.write_all(&ciphertext).expect("Error writing to output file");
     }
-
-
-    // println!("Input bytes: {:x?}", opt.input.as_bytes());
-
-    // println!("result: {:x?}", result);
-    // println!("result length: {:?}", result.len());
-    // println!("In c hexstring format");
-    // for byte in result.iter(){
-    //     print!("{:02x?}", byte);
-    // }
-    // println!("");
-    // let plaintext = opt.input;
-    // let plaintext = plaintext.as_bytes();
-    // println!("Testing encryption");
-    // print!("Plaintext: \n");
-    // for byte in plaintext.iter() {
-    //     print!("{:02x?}", byte);
-    // }
-    // println!("");
-    // println!("Plaintext len: {}", plaintext.len());
-    // let ad = vec![0; 16];
-    // let nonce = [0; 16];
-    // let key = [0; 32];
-    // println!("Encryption:");
-    // let e_data = gimli_aead_encrypt(plaintext, &ad, &nonce, &key);
-    // for byte in e_data.iter() {
-    //     print!("{:02x?}", byte);
-    // }
-    // println!("");
-    // println!("Ciphertext len: {}", e_data.len());
-    // println!("Decryption:");
-    // let d_data = gimli_aead_decrypt(&e_data, &ad, &nonce, &key);
-    // match d_data {
-    //     Ok(v) => {
-    //         for byte in v.iter() {
-    //             print!("{:02x?}", byte);
-    //         }
-    //         println!("");
-    //         println!("Decryption len: {:?}", v.len());
-    //     }
-    //     Err(e) => {
-    //         println!("Decrypt errored with {:?}", e);
-    //     }
-    // }
 }
